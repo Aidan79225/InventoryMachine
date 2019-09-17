9ec607d83373415137ffa300e1a60a78a9f658ee
Aidan Wang <aidan79225@gmail.com>
2019-09-17 23:15:15 +0800

    remove imei

--- scaner/app/src/main/java/com/aidan/inventoryworkplatform/KeyConstants.java
+++ scaner/app/src/main/java/com/aidan/inventoryworkplatform/KeyConstants.java
@@ -7,27 +7,7 @@ package com.aidan.inventoryworkplatform;
 public class KeyConstants {
     public static final String data = "DATA";
     public static final String isLogin = "isLogin";
-    public static final String[] IMEIS = {
-//            "353025075956037",
-//            "355693062107266",
-            "357640060110055",
-            "359409070925248",
-            "359409070923255",
-            "353025075956037",
-            "359409070923248",
-            "352842061132212",
-            "357640060110055",
-            "357579080044789",
-            "357579080044359",
-            "357579080064357",
-            "357579080215355",
-            "357579080260757",
-            "358240051111110",
-            "358276091125439",
-            "357579080285796",
-            "357579080214432",
-            "357579080540075"
-    };
+
     //---------------大標籤----------------------------------//
     public static final String AuthorityName = "法務部矯正署高雄";          //臺灣臺北地方法院檢察署
     public static final String ItemName = "非消耗物品";                    //非消耗物品
